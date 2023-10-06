3834259b707be98ba87e73a23398ecfe83b31342
Sention <tdangelov01@gmail.com>
2023-10-06 13:26:25 +0300

    main component

--- webpage/src/app/app.module.ts
+++ webpage/src/app/app.module.ts
@@ -10,11 +10,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
+    MainComponent,
 
   ],
   imports: [
