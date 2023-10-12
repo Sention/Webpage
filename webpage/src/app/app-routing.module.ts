ef576e3ac91ee859e889846dde1e545168e8e8cf
Sention <tdangelov01@gmail.com>
2023-10-12 10:43:19 +0300

    routing

--- webpage/src/app/app-routing.module.ts
+++ webpage/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ConnenctionsComponent } from './components/connenctions/connenctions.component';
 import { MainComponent } from './components/main/main.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   { path: 'about', component: AboutComponent},
   { path: 'connections', component: ConnenctionsComponent},
+  { path: 'portfolio', component: PortfolioComponent},
+  
 ];
 
 @NgModule({
--- webpage/src/app/app.module.ts
+++ webpage/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './components/about/about.component';
 import { ConnenctionsComponent } from './components/connenctions/connenctions.component';
 import { MainComponent } from './components/main/main.component';
 import { MatCardModule } from '@angular/material/card';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { MatCardModule } from '@angular/material/card';
     AboutComponent,
     ConnenctionsComponent,
     MainComponent,
+    PortfolioComponent,
   ],
   imports: [
     BrowserModule,
